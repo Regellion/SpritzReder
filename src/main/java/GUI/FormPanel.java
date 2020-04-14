719d72858a18c043885eb7bb7f54703810ddfbc2
Regellion <55495658+Regellion@users.noreply.github.com>
2020-04-14 19:18:56 +0300

    Add input form and fix errors
    добавлена вторая рабочая форма, реализован ввод данных 3-мя типами, добавлены слушатели

--- src/main/java/GUI/FormPanel.java
+++ src/main/java/GUI/FormPanel.java
@@ -3,7 +3,7 @@ package GUI;
 import javax.swing.*;
 
 // Абстрактный класс для форм и получения Панелей
-public abstract class FormPanel {
+abstract class FormPanel {
     JPanel getPanel(){
         return null;
     }
